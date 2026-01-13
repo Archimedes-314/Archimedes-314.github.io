f60c9d2d35780816d81796765b2007373cc788c8
Archimedes-314 <e.schodel3@gmail.com>
2026-01-13 13:02:32 +1000

    Delete load-menu.js

--- load-menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/important/menu.html")
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById("menu-container").innerHTML = html;
-    })
-    .catch(err => console.error("Menu failed to load:", err));
-});
