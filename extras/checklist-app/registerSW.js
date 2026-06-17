6fe10b66b4695095d34e3d035acb02ddc200a2f6
unknown <e.schodel3@gmail.com>
2026-06-17 10:15:51 +1000

    Updated: Checklist PWA

--- extras/checklist-app/registerSW.js
+++ extras/checklist-app/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/extras/checklist-app/dist/sw.js', { scope: '/extras/checklist-app/dist/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
