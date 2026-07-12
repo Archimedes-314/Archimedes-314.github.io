1d995683a7ebb2c689dd3fe0d633e47f0d39bd04
unknown <e.schodel3@gmail.com>
2026-07-12 19:44:20 +1000

    Added sync feature to Kanban app

--- extras/todo-app/registerSW.js
+++ extras/todo-app/registerSW.js
@@ -1,7 +1,1 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/extras/todo-app/sw.js", {
-      scope: "/extras/todo-app/",
-    });
-  });
-}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/extras/todo-app/sw.js', { scope: '/extras/todo-app/' })})}
