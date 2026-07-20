0035b38aa02e0c2eb2d84a5117e32cf1faaa4a42
unknown <e.schodel3@gmail.com>
2026-07-20 21:15:51 +1000

    Added: Reference Generator:

---
+++ extras/reference-generator/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/extras/reference-generator/sw.js', { scope: '/extras/reference-generator/' })})}
