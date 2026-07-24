7c185147a8d5d66a2fdaff82d181579929d3c41f
unknown <e.schodel3@gmail.com>
2026-07-24 22:05:50 +1000

    Added: work checklist app

---
+++ work-checklist-app/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/extras/work-checklist-app/sw.js', { scope: '/extras/work-checklist-app/' })})}
