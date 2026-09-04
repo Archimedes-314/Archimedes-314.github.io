b54b7620e1f1cd3e6ab52d2266d21d773277eedc
unknown <e.schodel3@gmail.com>
2026-09-05 06:31:02 +1000

    Updated: Reference generator PWA update logic

--- extras/reference-generator/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/extras/reference-generator/sw.js', { scope: '/extras/reference-generator/' })})}
